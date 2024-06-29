4fd4846b5e90eb611540898b00c848a49d0340dd
Kevin Moore <kevmo@Kevins-MacBook-Pro.local>
2024-06-28 20:31:19 -0400

    on tap change WIP

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,14 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack() {
-            Image(systemName: "link")
-                .imageScale(.large)
-                .foregroundColor(.green)
-            Text("Hello, biaa biaa!")
+        HStack() {
+//            Image(systemName: "link")
+//                .imageScale(.large)
+//                .foregroundColor(.green)
+//            Text("Hello, biaa biaa!")
+            QuarterBox(isFaceUp: true)
+            QuarterBox(isFaceUp: true)
+            QuarterBox(isFaceUp: false)
+            QuarterBox(isFaceUp: true)
         }
         .padding()
         .font(.largeTitle)
+    }
+}
+
+struct QuarterBox: View {
+    @State var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack { // this is a trailing closure
+            
+            let base = RoundedRectangle(cornerSize: /*@START_MENU_TOKEN@*/CGSize(width: 20, height: 10)/*@END_MENU_TOKEN@*/)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2, antialiased: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                Text("👻")
+            } else {
+                base.fill()
+                Text("🕳️")
+            }
+        }
+        .onTapGesture {
+            print("tapped")
+            isFaceUp = !isFaceUp
+        }
+
     }
 }
 
